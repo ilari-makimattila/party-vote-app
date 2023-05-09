8d374379896b839f7eac1d64100b32174b992483
Ilari Mäkimattila <ilari.makimattila@gmail.com>
2023-05-09 20:50:12 +0200

    Style back button

--- ui/src/routes/games/[slug]/+page.svelte
+++ ui/src/routes/games/[slug]/+page.svelte
@@ -69,7 +69,9 @@
 			</div>
 		</section>
 	{:else}
-		<a href="?item={data.game.items.length - 1}">Back to votes</a>
+        <a href="?item={data.game.items.length - 1}"
+            class="py-2.5 px-5 m-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >Back to votes</a>
 	{/if}
 	<Statistics gameId={data.game.id} />
 {:else}
